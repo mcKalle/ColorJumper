18ac5e4b8a51a56aa21e8154221f342316104ee5
pkalenda <philipp.kalenda@lieberlieber.com>
2017-05-17 08:34:32 +0200

    added jump improvement, camera follow and better movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,29 +6,60 @@ namespace Assets.Scripts
 	{
 
 		public float moveSpeed;
-		public float jumpHeight;
+		public float jumpVelocity;
+
+		public float fallMultiplier = 2.5f;
+		public float lowJumpMultiplier = 2f;
+
+		Rigidbody2D rigidbody;
+
+		void Awake()
+		{
+			rigidbody = GetComponent<Rigidbody2D>();
+		}
 
 		// Use this for initialization
-		void Start () {
-		
-		}
-	
-		// Update is called once per frame
-		void Update () {
+		void Start()
+		{
 
+		}
+
+		// Update is called once per frame
+		void FixedUpdate()
+		{
+			HandlingJump();
+
+			HandlingMovement();
+
+		}
+
+		void HandlingJump()
+		{
 			if (Input.GetKeyDown(KeyCode.Space))
 			{
-				GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpHeight);
-			}
-			if (Input.GetKeyDown(KeyCode.D))
-			{
-				GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, 0);
-			}
-			if (Input.GetKeyDown(KeyCode.A))
-			{
-				GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, 0);
+				rigidbody.velocity = Vector2.up * jumpVelocity;
 			}
 
+			// if falling downwards
+			if (rigidbody.velocity.y < 0)
+			{
+				// - 1 because unity physics already adds one time of the gravitx
+				// Time.deltaTime to match it to the frames
+
+				rigidbody.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+			}
+			// if jumping upward and not holding down the jump key
+			else if (rigidbody.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+			{
+				rigidbody.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+			}
+		}
+
+		void HandlingMovement()
+		{
+			float horizontal = Input.GetAxis("Horizontal");
+
+			rigidbody.velocity = new Vector2(horizontal * moveSpeed, rigidbody.velocity.y);
 		}
 	}
 }
