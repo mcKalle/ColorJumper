5d9efa80ed14de077757437c4cd7ceb5a35179f1
pkalenda <philipp.kalenda@lieberlieber.com>
2017-05-17 07:42:13 +0200

    added first test scene create sprites with sprite editor added first basic move script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+	public class PlayerController : MonoBehaviour
+	{
+
+		public float moveSpeed;
+		public float jumpHeight;
+
+		// Use this for initialization
+		void Start () {
+		
+		}
+	
+		// Update is called once per frame
+		void Update () {
+
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpHeight);
+			}
+			if (Input.GetKeyDown(KeyCode.D))
+			{
+				GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, 0);
+			}
+			if (Input.GetKeyDown(KeyCode.A))
+			{
+				GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, 0);
+			}
+
+		}
+	}
+}
