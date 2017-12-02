3990cb4bf09a7fa4763d184951fac86381289045
mcKalle <p.kalenda@gmx.at>
2017-12-02 17:59:33 +0100

    added TextMeshPro asset, introduce Menu GUI

---
+++ Assets/Scripts/MenuUIManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuUIManager : MonoBehaviour
+{
+
+   
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    
+}
