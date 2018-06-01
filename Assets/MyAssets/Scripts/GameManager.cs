071e51640138384b56446f6726d25265e92f6e8e
mcKalle <p.kalenda@gmx.at>
2018-06-01 17:29:45 +0200

    added various materials for light and dark elements

--- Assets/MyAssets/Scripts/GameManager.cs
+++ Assets/MyAssets/Scripts/GameManager.cs
@@ -10,8 +10,6 @@ using System.Linq;
 
 public class GameManager : MonoBehaviour
 {
-    public Camera MainCamera;
-
     [Header("Light Color")]
     public Color LightBackgroundColor;
     public Color LightColor2;
@@ -19,11 +17,12 @@ public class GameManager : MonoBehaviour
     public Color LightColor4;
     public Color LightPlayerColor;
     [Space]
-    public Color LightPowerUpColor;
-    [Space]
     public Material LightPlatformMaterial;
     [Space]
     public Material LightPlayerMaterial;
+    [Space]
+    public Color[] LightColorChangerColors;
+    public Material[] LightColorChangerMaterials;
 
     [Space]
     [Space]
@@ -35,24 +34,31 @@ public class GameManager : MonoBehaviour
     public Color DarkColor4;
     public Color DarkPlayerColor;
     [Space]
-    public Color DarkPowerUpColor;
-    [Space]
     public Material DarkPlatformMaterial;
     [Space]
     public Material DarkPlayerMaterial;
+    [Space]
+    public Color[] DarkColorChangerColors;
+    public Material[] DarkColorChangerMaterials;
 
     [Space]
     [Space]
 
     [Header("Obstacle Containers")]
-    public GameObject Color1Obstacles;
-    public GameObject Color2Obstacles;
+    public GameObject LightObstacles;
+    public GameObject DarkObstacles;
 
     [Space]
     [Space]
 
     [Header("Start Point")]
     public Transform LevelEntryPoint;
+
+    [Space]
+    [Header("Sprites")]
+    public Sprite PowerUpSprite;
+    public Sprite EnemySpriteBase;
+    public Sprite EnemySpriteFrame;
 
     [HideInInspector]
     public List<GameObject> powerUps;
@@ -114,7 +120,10 @@ public class GameManager : MonoBehaviour
     public void Respawn()
     {
         // hide death menu
-        uiManager.deathPanel.SetActive(false);
+        if (uiManager.deathPanel != null)
+        {
+            uiManager.deathPanel.SetActive(false);
+        }
 
         // reset position
         player.transform.position = LevelEntryPoint.position;
@@ -181,20 +190,27 @@ public class GameManager : MonoBehaviour
         switch (colorMode)
         {
             case 1:
-                MainCamera.backgroundColor = DarkBackgroundColor;
+                Camera.main.backgroundColor = DarkBackgroundColor;
                 playerRenderer.material = DarkPlayerMaterial;
-                Color1Obstacles.SetActive(false);
-                Color2Obstacles.SetActive(true);
+
+                SwitchColorChanger(DarkColorChangerColors, DarkColorChangerMaterials);
+
+
+                LightObstacles.SetActive(false);
+                DarkObstacles.SetActive(true);
                 if (uiManager != null)
                 {
                     uiManager.ApplyColorChange(colorMode);
                 }
                 break;
             case 2:
-                MainCamera.backgroundColor = LightBackgroundColor;
+                Camera.main.backgroundColor = LightBackgroundColor;
                 playerRenderer.material = LightPlayerMaterial;
-                Color1Obstacles.SetActive(true);
-                Color2Obstacles.SetActive(false);
+
+                SwitchColorChanger(LightColorChangerColors, LightColorChangerMaterials);
+
+                LightObstacles.SetActive(true);
+                DarkObstacles.SetActive(false);
                 if (uiManager != null)
                 {
                     uiManager.ApplyColorChange(colorMode);
@@ -213,6 +229,33 @@ public class GameManager : MonoBehaviour
         }
 
     }
+
+    private void SwitchColorChanger(Color[] colors, Material[] materials)
+    {
+        for (int i = 0; i < colors.GetLength(0); i++)
+        {
+            materials[i].color = colors[i];
+        }
+    }
+
+    //private void ChangeSprite(Sprite sprite)
+    //{
+    //    Color[] spriteColors = sprite.texture.GetPixels();
+
+    //    for (int i = 0; i < spriteColors.GetLength(0); i++)
+    //    {
+    //        Color pixelColor = spriteColors[i];
+
+    //        float color = IsLightColor ? 255f : 0f;
+    //        pixelColor.r = color;
+    //        pixelColor.g = color;
+    //        pixelColor.b = color;
+
+    //        spriteColors[i] = pixelColor;
+    //    }
+
+    //    sprite.texture.SetPixels(spriteColors);
+    //}
 
     #endregion
 
